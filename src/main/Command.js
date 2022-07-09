257751a86509af56a3399dce0940ce90714c65ae
xianyunleo <1040063186@qq.com>
2022-07-09 22:07:59 +0800

    代码提交

--- src/main/Command.js
+++ src/main/Command.js
@@ -1,19 +1,26 @@
 import child_process from "child_process";
 import sudo from "sudo-prompt"
 import {APP_NAME} from "@/main/constant";
+import is from "electron-is";
 
 export default class Command {
     static async exec(command) {
         return await new Promise(function (resolve, reject) {
-            let formatCommand = '@chcp 65001 >nul & cmd /d/s/c ';
-            command = formatCommand + command;
+            let formatCommand;
+            if (is.windows()) {
+                formatCommand = '@chcp 65001 >nul & cmd /d/s/c ';
+                command = formatCommand + command;
+            }
+
             const bufferEncoding = "utf8";
             const options = {
                 encoding: bufferEncoding
             }
             child_process.exec(command, options, (err, stdout, stderr) => {
                 if (err) {
-                    err = new Error(err.message.replace(formatCommand, ''))
+                    if (is.windows()) {
+                        err = new Error(err.message.replace(formatCommand, ''))
+                    }
                     reject(err);
                 } else if (stderr.lenght > 0) {
                     reject(new Error(stderr.toString()));
--- vue.config.js
+++ vue.config.js
@@ -16,22 +16,22 @@ module.exports ={
           ]
         },
         win:{
-          "target": [
+          target: [
             {
-              "target": "zip",
-              "arch": ["x64"]
+              target: "zip",
+              arch: ["x64"]
             },
             {
-              "target": "portable",
-              "arch": ["x64"]
+              target: "portable",
+              arch: ["x64"]
             }
           ],
           icon: 'build/icons/icon.ico',
         },
         artifactName:'${productName}-${os}-${version}.${ext}',
-        "extraFiles": {
-          "from": "./extra/win/",
-          "to": "./",
+        extraFiles: {
+          from: "./extra/win/",
+          to: "./",
         },
       }
     }
