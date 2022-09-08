e3c70817c6308d15405135f85c7ebe754fe9986d
xianyunleo <1040063186@qq.com>
2022-09-08 10:29:47 +0800

    php7.4更新

--- extra/darwin/Core/software/php/php-7.4/include/php/main/php_config.h
+++ extra/darwin/Core/software/php/php-7.4/include/php/main/php_config.h
@@ -2054,7 +2054,7 @@
 #define PHPDBG_DEBUG 0
 
 /* PHP build date */
-#define PHP_BUILD_DATE "2022-09-07"
+#define PHP_BUILD_DATE "2022-09-08"
 
 /* Define if your system has fork/vfork/CreateProcess */
 #define PHP_CAN_SUPPORT_PROC_OPEN 1
