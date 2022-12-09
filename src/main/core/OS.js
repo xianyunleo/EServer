c3b1beed39ecd341edebbfac2f370da56a7c14ba
xianyunleo <1040063186@qq.com>
2022-12-09 15:14:57 +0800

    服务，启动，停止，重启优化

--- src/main/core/OS.js
+++ src/main/core/OS.js
@@ -2,6 +2,8 @@ import * as os from "os";
 import is from "electron-is";
 
 export default class OS {
+    static _isMacOSLowVersion = null;
+
     static getReleaseVersion() {
         return os.release();
     }
@@ -16,5 +18,23 @@ export default class OS {
 
     static isWindows() {
         return is.windows();
+    }
+
+    static isMacOSLowVersion() {
+        if (OS._isMacOSLowVersion != null) {
+            return OS._isMacOSLowVersion;
+        }
+
+        if (OS.isMacOS()) {
+            let mainVersion = OS.getReleaseVersion().split('.')[0];
+            //https://en.wikipedia.org/wiki/Darwin_(operating_system)
+            //<=macOS 10.13 High Sierra
+            if (Number(mainVersion) <= 17) {
+                OS._isMacOSLowVersion = true;
+                return true;
+            }
+        }
+        OS._isMacOSLowVersion = false;
+        return false;
     }
 }
