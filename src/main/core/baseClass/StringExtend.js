4e88a9c32b0fc5e7a81ce41a6d3663036902001d
xianyunleo <1040063186@qq.com>
2023-02-03 15:07:39 +0800

    去除多余注释

--- src/main/core/baseClass/StringExtend.js
+++ src/main/core/baseClass/StringExtend.js
@@ -6,7 +6,6 @@ export default class StringExtend {
     static init() {
         //将字符串里的所有反斜杠替换成正斜杠
         String.prototype.replaceSlash = function () {
-            console.log('456')
             return this.replaceAll("\\", "/")
         };
     }
