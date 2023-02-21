ecac415abff75c1c602476a2d5fa61eb4ee2ad7d
xianyunleo <1040063186@qq.com>
2023-02-21 17:49:01 +0800

    TcpProcess方法完善

--- src/main/core/TcpProcess.js
+++ src/main/core/TcpProcess.js
@@ -1,4 +1,5 @@
 import Command from "@/main/core/Command";
+import OS from "@/main/core/OS";
 
 export default class TcpProcess {
 
@@ -9,8 +10,22 @@ export default class TcpProcess {
      */
     static async getPidByPort(port) {
         try {
-            let commandStr = `PowerShell -Command "& {(Get-NetTCPConnection -LocalPort ${port} -State Listen).OwningProcess)}"`;
-            let pid = await Command.exec(commandStr);
+            let pid, commandStr;
+
+            if (OS.isWindows()) {
+                commandStr = `PowerShell -Command "& {(Get-NetTCPConnection -LocalPort ${port} -State Listen).OwningProcess}"`;
+                pid = await Command.exec(commandStr);
+                if (!pid) {
+                    return null;
+                }
+            } else {
+                commandStr = `lsof -t -sTCP:LISTEN -i:${port}`;
+                let str = await Command.exec(commandStr);
+                if (!str) {
+                    return null;
+                }
+                pid = str.trim().split("\n")[0];
+            }
             return parseInt(pid.trim());
         } catch {
             return null;
@@ -23,8 +38,22 @@ export default class TcpProcess {
      */
     static async getPathByPort(port) {
         try {
-            let commandStr = `PowerShell -Command "& {(Get-Process -Id (Get-NetTCPConnection -LocalPort ${port} -State Listen).OwningProcess).path}"`;
-            let path = await Command.exec(commandStr);
+            let path, commandStr;
+
+            if (OS.isWindows()) {
+                commandStr = `PowerShell -Command "& {(Get-Process -Id (Get-NetTCPConnection -LocalPort ${port} -State Listen).OwningProcess).path}"`;
+                path = await Command.exec(commandStr);
+                if (!path) {
+                    return null;
+                }
+            } else {
+                commandStr = `lsof -a -d txt -p ${port}|awk '{print $9}'`;
+                let str = await Command.exec(commandStr);
+                if (!str) {
+                    return null;
+                }
+                path = str.trim().split("\n")[1];
+            }
             return path.trim();
         } catch {
             return null;
