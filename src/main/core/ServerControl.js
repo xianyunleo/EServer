92d445c2db9a4de82674fe52e2e6add77168856a
xianyunleo <1040063186@qq.com>
2023-02-22 15:12:02 +0800

    ServerControl的start方法优化

--- src/main/core/ServerControl.js
+++ src/main/core/ServerControl.js
@@ -21,7 +21,7 @@ export default class ServerControl {
         const item = softItem;
         let workPath = Software.getPath(item); //服务目录
         let serverProcessPath = Path.Join(workPath, item.ServerProcessPath);  //服务的进程目录
-        const options = {cwd: workPath};
+        const options = {cwd: workPath, detached: true};
 
         if (!File.Exists(serverProcessPath)) {
             throw new Error(`${serverProcessPath} 文件不存在！`);
@@ -41,16 +41,24 @@ export default class ServerControl {
                 return parseTemplateStrings(arg, argObj);
             })
         }
+
         item.isRunning = true;
         item.errMsg = '';
-        let childProcess = child_process.execFile(serverProcessPath, args, options, (error, stdout, stderr) => {
-            item.isRunning = false;
-            if (stderr) {
-                item.errMsg = stderr;
-            }
+
+        let childProcess = child_process.spawn(serverProcessPath, args, options);
+
+        childProcess.stderr.on('data', (data) => {
+            console.log('stderr data',data?.toString())
+            item.errMsg = data?.toString();
         });
 
-        if(App.isDev()) console.log('ServerControl start command:',`${serverProcessPath} ${args.join(' ')}`)
+        childProcess.on('close', (code) => {
+            if (App.isDev()) console.log(`${serverProcessPath},exit code ${code}`);
+            item.isRunning = false;
+        });
+
+        if (App.isDev()) console.log('ServerControl start command:', `${serverProcessPath} ${args.join(' ')}`);
+        if (App.isDev()) console.log(`${serverProcessPath},pid ${childProcess.pid}`);
 
         item.pid = childProcess.pid;
         if (item.ManualWriteServerPid && item.pid) {
