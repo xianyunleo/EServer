8ab335e912e16135429be02dd37e8f64f74c6140
xianyunleo <liu1040063186@gmail.com>
2023-03-01 16:34:52 +0800

    Windows平台，打开软件时，自动停止IIS Service

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -23,6 +23,8 @@ import UserPwdModal from "@/renderer/components/UserPwdModal.vue";
 import OS from "@/main/core/OS";
 import SettingsExtend from "@/main/core/SettingsExtend";
 import Software from "@/main/core/software/Software";
+import Service from "@/main/core/Service";
+import {message} from "ant-design-vue";
 
 const userPwdModalShow = ref(false);
 
@@ -35,6 +37,10 @@ provide('globalSpinning',spinning);
   }catch (error){
     await MessageBox.error(error.message ?? error, '软件出错！');
     App.exit();
+  }
+
+  if (OS.isWindows()) {
+    stopWebService();
   }
 
   if (!App.initFileExists() || App.isDev()) {
@@ -55,8 +61,15 @@ provide('globalSpinning',spinning);
     }
   }
 
-
 })()
+
+async function stopWebService() {
+  const IISServiceName = 'W3SVC';
+  if (await Service.isRunning(IISServiceName)) {
+    await Service.stop(IISServiceName);
+    message.info('已自动停止IIS服务');
+  }
+}
 
 </script>
 
