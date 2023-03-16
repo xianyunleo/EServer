9966da652f074fa6e5e3e7e1b067663793345ce5
xianyunleo <liu1040063186@gmail.com>
2023-03-16 18:09:04 +0800

    temp目录从'tmp'改成'temp'

--- src/main/constant.js
+++ src/main/constant.js
@@ -1,6 +1,8 @@
 export const WIN_CORE_PATH_NAME = 'core'
 export const MAC_CORE_PATH_NAME = 'Core'
 export const MAC_USER_CORE_PATH = '/Applications/EasySrv/'
+
+export const TEMP_DIR_NAME = 'temp'
 export const INIT_FILE_NAME = 'init.txt'
 export const InitFiles_DIR_NAME = 'initFiles'
 export const SETTINGS_FILE_NAME = 'settings'
