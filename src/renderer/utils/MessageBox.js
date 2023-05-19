43ba2727b9bd5731b50cd45042c86b169cde74f4
xianyunleo <liu1040063186@gmail.com>
2023-05-19 16:40:58 +0800

    MessageBox下的方法优化，支持\n换行

--- src/renderer/utils/MessageBox.js
+++ src/renderer/utils/MessageBox.js
@@ -1,11 +1,18 @@
 import {Modal} from 'ant-design-vue';
+import {createVNode} from "vue";
 
 export default class MessageBox {
+    /**
+     *
+     * @param message {string}
+     * @param title {string}
+     * @returns {Promise<boolean>}
+     */
     static async info(message, title) {
         title = title ?? '信息提示';
         let options = {
             title: title,
-            content: message,
+            content: createVNode('div', {innerHTML: message.replaceAll("\n", "<br/>")})
         }
         let result = true;
 
@@ -25,11 +32,17 @@ export default class MessageBox {
         return result;
     }
 
+    /**
+     *
+     * @param message {string}
+     * @param title {string}
+     * @returns {Promise<boolean>}
+     */
     static async error(message, title) {
         title = title ?? '错误提示';
         let options = {
             title: title,
-            content: message,
+            content: createVNode('div', {innerHTML: message.replaceAll("\n", "<br/>")})
         }
         let result = true;
 
@@ -49,11 +62,17 @@ export default class MessageBox {
         return result;
     }
 
+    /**
+     *
+     * @param message {string}
+     * @param title {string}
+     * @returns {Promise<boolean>}
+     */
     static async warning(message, title) {
         title = title ?? '警告提示';
         let options = {
             title: title,
-            content: message,
+            content: createVNode('div', {innerHTML: message.replaceAll("\n", "<br/>")})
         }
         let result = true;
 
@@ -74,11 +93,14 @@ export default class MessageBox {
     }
 
     /**
-     * @param options
+     * @param options {object}
      * @returns {Promise<boolean>}
      */
     static async confirm(options={}) {
         options.title = options.title ?? '提示';
+        if (typeof options.content === 'string') {
+            options.content = createVNode('div', {innerHTML: options.content?.replaceAll("\n", "<br/>")})
+        }
         let result = true;
 
         await new Promise((resolve, reject) => {
