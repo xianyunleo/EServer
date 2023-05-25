01c8ceb80e9b123a7c7357a80ba6f2797c5836a1
xianyunleo <liu1040063186@gmail.com>
2023-05-25 16:31:37 +0800

    disabled属性使用serverTableLoading时，强转成bool类型

--- src/renderer/views/Home.vue
+++ src/renderer/views/Home.vue
@@ -2,8 +2,8 @@
   <div class="content-container">
     <a-card title="快捷操作" class="quick-card">
       <div class="quick-card-content">
-        <a-button type="primary" @click="oneClickStart" :disabled="serverTableLoading">一键启动</a-button>
-        <a-button type="primary" @click="oneClickStop" :disabled="serverTableLoading">一键停止</a-button>
+        <a-button type="primary" @click="oneClickStart" :disabled="!!serverTableLoading">一键启动</a-button>
+        <a-button type="primary" @click="oneClickStop" :disabled="!!serverTableLoading">一键停止</a-button>
         <a-button type="primary" @click="corePathClick">{{APP_NAME}}目录</a-button>
         <a-button type="primary" @click="wwwPathClick">网站目录</a-button>
       </div>
