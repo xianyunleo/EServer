6ab803bd4548b810ab21bad856ed2a42f701839d
xianyunleo <xianyunleo@gmail.com>
2023-11-02 18:18:24 +0800

    2.0.0

--- extra/darwin/Core/initFiles/software/php/php-7.4/include/php/ext/iconv/php_php_iconv_h_path.h
+++
@@ -1,1 +0,0 @@
-#define PHP_ICONV_H_PATH </Applications/EServer/Library/libiconv/include/iconv.h>
--- extra/darwin/Core/initFiles/software/tool/phpMyAdmin/vendor/pragmarx/google2fa-qrcode/src/Exceptions/MissingQrCodeServiceException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace PragmaRX\Google2FAQRCode\Exceptions;
-
-use Exception;
-
-class MissingQrCodeServiceException extends Exception
-{
-}
