59f05fa8242a29e724ccda26ce0edada7a855205
xianyunleo <xianyunleo@gmail.com>
2025-02-25 18:52:09 +0800

    更新childApp远程下载url

--- src/main/core/childApp/Installer.js
+++ src/main/core/childApp/Installer.js
@@ -142,7 +142,7 @@ export default class Installer extends EventEmitter {
             if (isWindows) {
                 url = `${url}/win`;
             } else {
-                url = `${url}/v3_mac_x64`;
+                url = `${url}/mac_x64`;
             }
         }
 
