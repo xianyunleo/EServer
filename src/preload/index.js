09334059b1391bb10f9d90660070bdd01eb14b71
xianyunleo <xianyunleo@gmail.com>
2025-03-15 16:56:16 +0800

    将preload中IpcRenderer相关函数，剥离到Ipc.js文件中

--- src/preload/index.js
+++ src/preload/index.js
@@ -1,20 +1,8 @@
-import { contextBridge, ipcRenderer } from 'electron'
+import { contextBridge } from 'electron'
 import { electronAPI } from '@electron-toolkit/preload'
 
 // Custom APIs for renderer
-const api = {
-    call: async (funName, ...args) => {
-        return await ipcRenderer.invoke('call', funName, ...args)
-    },
-    callStatic: async (className, methodName, ...args) => {
-        return await ipcRenderer.invoke('callStatic', className, methodName, ...args)
-    },
-    onMainWindowMaximize: (callback) => ipcRenderer.on('mainWindowMaximize', (_event, ...args) => callback(...args)),
-    onMainWindowUnmaximize: (callback) => ipcRenderer.on('mainWindowUnmaximize', (_event, ...args) => callback(...args)),
-    onAppDlProgress: (callback) => ipcRenderer.on('childApp-downloadProgress', (_event, ...args) => callback(...args)),
-    onAppDlCancelled: (callback) => ipcRenderer.on('childApp-downloadCancelled', (_event, ...args) => callback(...args)),
-    onAppInstStatus: (callback) => ipcRenderer.on('childApp-installStatus', (_event, ...args) => callback(...args))
-}
+const api = {}
 // Use `contextBridge` APIs to expose Electron APIs to
 // renderer only if context isolation is enabled, otherwise
 // just add to the DOM global.
---
+++ src/renderer/utils/Ipc.js
@@ -0,0 +1,19 @@
+import { onUnmounted } from 'vue'
+
+export default class Ipc {
+    static async call(funName, ...args) {
+        return await window.electron.ipcRenderer.invoke('call', funName, ...args)
+    }
+
+    static async callStatic(className, methodName, ...args) {
+        return await window.electron.ipcRenderer.invoke('callStatic', className, methodName, ...args)
+    }
+
+    static async on(channel, callback) {
+        const listener = (_event, ...args) => callback(...args)
+        onUnmounted(() => {
+            window.electron.ipcRenderer.removeListener(channel, listener)
+        })
+        return window.electron.ipcRenderer.on(channel, listener)
+    }
+}
