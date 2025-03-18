dbf5ec13f7aa447f65569a734335483e32324472
xianyunleo <xianyunleo@gmail.com>
2025-03-18 18:39:09 +0800

    fix getFileIcon for TcpProcess

--- src/shared/utils/file.js
+++ src/shared/utils/file.js
@@ -1,6 +1,7 @@
 export async function getFileIcon(path, options = {}) {
     if (process.type === 'renderer') {
-        return (await window.api.call('fileGetIcon', path, options)).toDataURL()
+        const Ipc = (await import('@/renderer/utils/Ipc')).default
+        return (await Ipc.call('fileGetIcon', path, options)).toDataURL()
     } else {
         const { app } = require('electron')
         return (await app.getFileIcon(path, options)).toDataURL()
