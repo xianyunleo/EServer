a55aaef0bf4002b2e7d7c20b53f3e1938cfd2d58
xianyunleo <xianyunleo@gmail.com>
2025-04-30 18:32:23 +0800

    '7zip-min-electron' 备注目前只在Windows上使用

--- src/main/utils/extract.js
+++ src/main/utils/extract.js
@@ -5,9 +5,8 @@ export async function extractZip(path, dest) {
     return await extract(path, {dir: dest});
 }
 
-
 export async function extract7z(path, dest) {
-    const _7z = require('7zip-min-electron');
+    const _7z = require('7zip-min-electron') //目前只在Windows上使用
     return new Promise((resolve, reject) => {
         _7z.unpack(path, dest, (err) => {
             err ? reject(err) : resolve(true);
