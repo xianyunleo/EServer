e5a6e4845f13e745a22ee623d764ba11b68e1635
xianyunleo <xianyunleo@gmail.com>
2025-04-30 18:32:06 +0800

    关于页，添加arch架构信息

--- src/renderer/views/About.vue
+++ src/renderer/views/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="content-container color-text">
     <p style="text-align: center; font-size: 18px; margin-top: 50px">{{ APP_NAME }}</p>
-    <p style="text-align: center">{{ $t('Version') }}：{{ version }}</p>
+    <p style="text-align: center">{{ $t('Version') }}：{{ version }} {{process.arch}}</p>
     <p style="text-align: center">
       {{ t('OfficialSite') }}：<a @click="openUrl(OFFICIAL_URL)">{{ OFFICIAL_HOST }}</a>
     </p>
@@ -22,6 +22,7 @@ import Native from '@/renderer/utils/Native'
 import { t } from '@/renderer/utils/i18n'
 import {onMounted, ref} from 'vue'
 import Ipc from '@/renderer/utils/Ipc'
+import process from 'process'
 
 const version = ref('')
 onMounted(async () => {
