fcf890e3c9264db086e456884a3ac6bf4d140ca7
xianyunleo <xianyunleo@gmail.com>
2025-05-07 19:00:04 +0800

    子应用已存在etc目录，安装时，不会修改配置文件

--- src/main/services/childApp/CommonInstall.js
+++ src/main/services/childApp/CommonInstall.js
@@ -2,6 +2,7 @@ import ChildAppExtend from "@/main/services/childApp/ChildAppExtend";
 import ChildAppInit from "@/main/services/childApp/ChildAppInit";
 import DirUtil from "@/main/utils/DirUtil";
 import { extract7z, extractTar, extractZip } from '@/main/utils/extract'
+import GetDataPath from '@/shared/utils/GetDataPath'
 
 export default class CommonInstall {
     static async extract(filePath, dest) {
@@ -19,16 +20,20 @@ export default class CommonInstall {
 
     static async configure(appItem) {
         await ChildAppInit.copyFiles(appItem)
+        const ownEtcExists = await DirUtil.Exists(GetDataPath.getOwnEtcDir(appItem.DirName))
         await ChildAppInit.initEtcFiles(appItem)
-        const dirName = appItem.DirName
-        if (dirName.match(/^mysql-[.\d]+$/)) {
-            const version = ChildAppExtend.getMysqlVersion(dirName)
-            await ChildAppInit.initMySQL(version)
-        } else if (dirName.match(/^php-[.\d]+$/)) {
-            const version = ChildAppExtend.getPHPVersion(dirName)
-            await ChildAppInit.initPHP(version)
-        } else if (dirName.match(/^nginx$/)) {
-            await ChildAppInit.initNginx()
+
+        if (!ownEtcExists) { //虽然一开始ownEtc不存在，但是initEtcFiles会创建ownEtc
+            const dirName = appItem.DirName
+            if (dirName.match(/^mysql-[.\d]+$/)) {
+                const version = ChildAppExtend.getMysqlVersion(dirName)
+                await ChildAppInit.initMySQL(version)
+            } else if (dirName.match(/^php-[.\d]+$/)) {
+                const version = ChildAppExtend.getPHPVersion(dirName)
+                await ChildAppInit.initPHP(version)
+            } else if (dirName.match(/^nginx$/)) {
+                await ChildAppInit.initNginx()
+            }
         }
     }
 }
