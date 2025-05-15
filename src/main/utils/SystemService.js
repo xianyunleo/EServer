bcb3ba7c7fb95c5c7d0360ce22a04ae2ba83082f
xianyunleo <xianyunleo@gmail.com>
2025-05-15 18:50:21 +0800

    SystemService新增create和exists方法

--- src/main/utils/SystemService.js
+++ src/main/utils/SystemService.js
@@ -1,6 +1,7 @@
 import Shell from '@/main/utils/Shell'
 import { isWindows } from '@/main/utils/utils'
 import { PowerShell } from '@/main/utils/constant'
+import nodePath from 'path'
 
 export default class SystemService {
     static async isRunning(name) {
@@ -17,6 +18,40 @@ export default class SystemService {
             }
         } catch {
             return false;
+        }
+    }
+
+    static async create(name, path) {
+        try {
+            path = nodePath.normalize(path)
+            let commandStr
+
+            if (isWindows) {
+                commandStr = `sc create ${name} binPath=${path}`
+                await Shell.exec(commandStr)
+                // eslint-disable-next-line no-empty
+            } else {
+
+            }
+            // eslint-disable-next-line no-empty
+        } catch {
+
+        }
+    }
+
+    static async exists(name) {
+        try {
+            let commandStr
+
+            if (isWindows) {
+                commandStr = `sc query ${name}`
+                await Shell.exec(commandStr)
+                // eslint-disable-next-line no-empty
+            } else {
+            }
+            // eslint-disable-next-line no-empty
+        } catch (e){
+            return false
         }
     }
 
