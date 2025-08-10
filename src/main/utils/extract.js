4e549999406c92813335f19230edd51459aa0170
xianyunleo <xianyunleo@gmail.com>
2025-08-10 23:13:03 +0800

    extractTarXz方法优化

--- src/main/utils/extract.js
+++ src/main/utils/extract.js
@@ -1,22 +1,21 @@
 import extract from 'extract-zip'
-import * as fs from 'fs'
+import * as fsPromises from 'fs/promises'
 
 export async function extractZip(path, dest) {
-    return await extract(path, {dir: dest});
+    return await extract(path, { dir: dest })
 }
 
 export async function extractTarXz(path, dest) {
-    const lzma = require('lzma-native')
+    const lzma = require('@napi-rs/lzma/xz')
+    const buffer = await fsPromises.readFile(path)
+    const buffer2 = await lzma.decompress(buffer)
     const tar = require('tar')
     const { pipeline } = require('stream/promises')
+    const { Readable } = require('node:stream')
 
     await pipeline(
         // 1. 读取.xz文件
-        fs.createReadStream(path),
-
-        // 2. 使用lzma-native解压xz流
-        lzma.createDecompressor(),
-
+        Readable.from(buffer2),
         // 3. 将解压后的tar流直接解包到目标目录
         tar.extract({ cwd: dest })
     )
